d5b2eff3098a965abe83658b61f2ef2465053ca0
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-02-22 17:59:22 +0500

    Assignment 5

---
+++ Assignment 5/main.ts
@@ -0,0 +1,13 @@
+
+//Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name.
+ //Make sure you use each character combination, "\t" and "\n", at least once. Print the name once,
+  //so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+
+  let person= "\t Ahmed \n Shah \t";
+
+  console.log("Name with whitespace: '" + person + "'");
+  
+ 
+  let strippedName: string = person.trim();
+  
+  console.log("Stripped name: '" + strippedName + "'");
