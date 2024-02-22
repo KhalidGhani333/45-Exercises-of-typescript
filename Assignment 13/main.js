39a1371cf755164bb321b7e3ba7926556d37f283
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-02-22 18:23:31 +0500

    Assignment 13

---
+++ Assignment 13/main.js
@@ -0,0 +1,10 @@
+"use strict";
+//Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite?
+// Make a list that includes at least three people you’d like to invite to dinner.
+// Then use your list to print a message to each person, inviting them to dinner.
+let invitation = ["Kashif", "Nadeem", "Tariq", "Ali"];
+console.log("Dear Mr." + invitation[0] + ", You are cordially invited to a dinner party at my place. Join us for a night of great food and even better company. ");
+console.log("Dear Mr." + invitation[1] + ", I sincerely invite you to join us for a lovely dinner. I hope to see you and have a wonderful time together.");
+console.log("Dear Mr." + invitation[2] + ", Good friend, I'm inviting you to the family dinner Friday at my house.");
+console.log("Dear Mr." + invitation[3] + " ,I'm inviting you to my dinner event tonight, beloved friend.");
+ 
