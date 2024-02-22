95c5b435724819a58215c2ce11b2aa2a627a6846
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-02-22 23:53:04 +0500

    Assignment 44

---
+++ Assignment 44/main.js
@@ -0,0 +1,12 @@
+"use strict";
+//Cars: Write a function that stores information about a car in a Object. The function should always
+// receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments.
+// Call the function with the required information and two other name-value pairs, such as a color or an optional feature.
+// Print the Object that’s returned to make sure all the information was stored correctly.
+function createCar(manufacturer, modelName, ...options) {
+    let carInfo = Object.assign({ manufacturer,
+        modelName }, Object.assign({}, ...options));
+    return carInfo;
+}
+let carDetails = createCar('Toyota', 'Camry', { color: 'Blue', features: ['Sunroof', 'Navigation'] });
+console.log(carDetails);
