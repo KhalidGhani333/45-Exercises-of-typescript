f675193874b868f1d8d612acdeeadc0e30adfff7
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-02-22 18:21:45 +0500

    Assignment 12

---
+++ Assignment 12/main.js
@@ -0,0 +1,9 @@
+"use strict";
+//Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car,
+// and make a list that stores several examples. Use your list to print a series of statements about these items,
+//  such as “I would like to own a Honda motorcycle.”
+let myown = ["Car", "House", "Pakistan tour", "hajj"];
+console.log("I wish,I have a Toyota corolla " + myown[0]);
+console.log("I would like to buy a luxury " + myown[1]);
+console.log("I go to " + myown[2] + " on my own car");
+console.log("I would like to go for " + myown[3] + " on my own " + myown[0]);
