05c18b343d94d30a77c2afaec4de9ca4943cbc70
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-02-22 23:16:57 +0500

    Assignment 18

---
+++ Assignment 18/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18,
+// print a message indicating the number of people you are inviting to dinner.
+let guest = 10;
+let message = " peoples are inviting for dinner in exercise 16 ";
+console.log(guest + message);
