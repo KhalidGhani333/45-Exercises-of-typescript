ef7447e5cb1c72c144ee341b24b941dd7cf703a2
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-02-22 18:19:59 +0500

    Assignment 11

---
+++ Assignment 11/main.ts
@@ -0,0 +1,12 @@
+
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name,
+ //print a message to them. The text of each message should be the same,
+ // but each message should be personalized with the person’s name
+
+
+ let names:string[]=["Kashif","Nadeem","Tariq","Mairaj"]
+
+console.log("Dear "+names[0]+ " can you go with us for picnic ")
+console.log("Dear "+names[1]+ " can you go with us for picnic ")
+console.log("Dear "+names[2]+ " can you go with us for picnic ")
+console.log("Dear "+names[3]+ " can you go with us for picnic ")
