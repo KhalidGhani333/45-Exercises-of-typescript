178366364bf8f9c7c49a03b5c09a4fb2cfad0a8e
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-02-22 18:01:09 +0500

    Assignment 6

---
+++ Assignment 6/main.ts
@@ -0,0 +1,19 @@
+
+//Number Eight: Write addition, subtraction, multiplication, and division operations that
+// each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+let a=4
+let b=4
+console.log(a+b)
+
+let c=10
+let d=2
+console.log(c-d)
+
+let e=4
+let f=2
+console.log(e*f)
+
+let g=16
+let h=2
+console.log(g/h)
