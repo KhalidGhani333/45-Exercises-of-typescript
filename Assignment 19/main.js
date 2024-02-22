70d0561b127b9f20968cbd23d36ba2e9e68a6dc6
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-02-22 23:18:34 +0500

    Assignment 19

---
+++ Assignment 19/main.js
@@ -0,0 +1,10 @@
+"use strict";
+//Think of something you could store in a array. For example, you could make a list of mountains,
+// rivers, countries, cities, languages, or anything else you’d like.
+// Write a program that creates a list containing these items.
+let cities = ["Karachi", "Islamabad", "Lahore", "Rawalpindi", "Peshawar", "Sialkot", "Hyderabad", "Quettta", "Sukkur",
+    "Faislabad", "Multan"];
+console.log(cities);
+for (let i = 0; i < 11; i++) {
+    console.log(cities[i]);
+}
