9c06aa46d9ad7d20be353a373f07cedd49b1f9bc
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-02-22 18:07:14 +0500

    Assignment 8

---
+++ Assignment 8/main.js
@@ -0,0 +1,5 @@
+"use strict";
+//Favorite Number: Store your favorite number in a variable. Then, using that variable,
+//create a message that reveals your favorite number. Print that message.
+let num = 56;
+console.log("I like Babar Azam.The jursey number of babar is " + num + " that why this is my favourite number");
