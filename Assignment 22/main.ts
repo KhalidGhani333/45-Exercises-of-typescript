546e680bdb55d35954961d01567d9250e3cf3e48
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-02-22 23:23:24 +0500

    Assignment 22

---
+++ Assignment 22/main.ts
@@ -0,0 +1,51 @@
+//Conditional Tests: Write a series of conditional tests. Print a statement describing each test
+// and your prediction for the results of each test.
+
+
+// Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+//Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+
+let car="subaru"
+
+console.log("Is car == 'subaru'? I predict True")
+console.log(car=='subaru')
+
+console.log("Is car == subaru? I predict False ")
+console.log(car!=='subaru')
+
+console.log("is car == 'toyota'? I predict True")
+console.log(car !=='toyota')
+
+console.log("is car == 'toyota'? I predict false")
+console.log(car =='toyota')
+
+let a=5
+let b=6
+console.log("a+b=11? I predict True")
+console.log(a+b==11)
+
+console.log("a+b=12? I predict False")
+console.log(a+b==12)
+
+let c=8
+let d=10
+console.log("c*d= 80? I predict True")
+console.log(c*d ==80)
+
+console.log("c*d= 80? I predict False")
+console.log(c*d!==80)
+
+let e=9
+let f=3
+
+console.log("e>f ? I predict True")
+console.log(e > f)
+
+console.log("e < f? I predict false")
+console.log(e < f)
+
+
+
+
