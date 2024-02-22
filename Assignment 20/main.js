cbca9cf4ade813356b2c19a0f6a3ff180a4057ab
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-02-22 23:20:03 +0500

    Assignment 20

---
+++ Assignment 20/main.js
@@ -0,0 +1,17 @@
+"use strict";
+//They think of something you could store in a TypeScript Object.
+// Write a program that creates Objects containing these items.
+let student1 = {
+    StudentName: "Bilal Ahmed",
+    age: 22,
+    city: "karachi",
+    RollNumber: 1234
+};
+let student2 = {
+    StudentName: "M.Hassan",
+    age: 25,
+    city: "Hyderabad",
+    RollNumber: 5678
+};
+console.log(student1);
+console.log(student2);
---
+++ Assignment 20/main.ts
@@ -0,0 +1,22 @@
+//They think of something you could store in a TypeScript Object.
+// Write a program that creates Objects containing these items.
+
+ type std={StudentName:string,age:number,city:string,RollNumber:number}
+
+ let student1:std ={
+  StudentName:"Bilal Ahmed",
+  age:22,
+  city:"karachi",
+  RollNumber:1234
+}
+
+let student2:std ={
+  StudentName:"M.Hassan",
+  age:25,
+  city:"Hyderabad",
+  RollNumber:5678
+}
+
+console.log(student1)
+console.log(student2)
+
